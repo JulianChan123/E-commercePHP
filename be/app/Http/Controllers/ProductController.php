72d0f501990e42caa347a13c1c3004894502a495
Julian Chan Palomo <73038870+JulianChan123@users.noreply.github.com>
2023-05-27 12:48:49 -0600

    productAPI

--- be/app/Http/Controllers/ProductController.php
+++ be/app/Http/Controllers/ProductController.php
@@ -6,8 +6,7 @@ use App\Models\product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-class ProductController extends Controller
-{
+class ProductController extends Controller{
     public function add(Request $request){
         
         $validator=Validator::make($request->all(),[
@@ -43,5 +42,57 @@ class ProductController extends Controller
 
         
     }
+    public function update(Request $request){
+        
+        $validator=Validator::make($request->all(),[
+            'name'=>'required',
+            'category'=>'required',
+            'brand'=>'required',
+            'desc'=>'required',
+            'id'=>'required',
+            'price'=>'required',
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json(['error'=>$validator->errors()->all()], 409);
+        }
+
+        $product = product::find($request->id);
+        $product->name = $request->name;
+        $product->category = $request->category;
+        $product->brand = $request->brand;
+        $product->desc = $request->desc;
+        $product->price = $request->price;
+        $product->save();
+        return response()->json(['message'=>"Product succesfully Updated"]);
+        
+    }
+    public function delete(Request $request){
+        
+        $validator=Validator::make($request->all(),[
+            'id'=>'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error'=>$validator->errors()->all()], 409);
+        }
+
+        $product = product::find($request->id)->delete();
+   
+        return response()->json(['message'=>"Product succesfully Deleted"]);
+        
+    }
+
+    public function get(Request $request){
+        session(['keys'=>$request->keys]);
+        $products=product::where(function ($query){
+            $query->where('products.id','LIKE','%'.session('keys').'%')
+            ->orwhere('products.name','LIKE','%'.session('keys').'%')
+            ->orwhere('products.price','LIKE','%'.session('keys').'%')
+            ->orwhere('products.category','LIKE','%'.session('keys').'%')
+            ->orwhere('products.brand','LIKE','%'.session('keys').'%');
+
+        })->select('products.*')->get();
+        return response()->json(['products'=>$products]);
+    }
 }
