61109152dd85827a09eceff9b6e26472ae46151e
Julian Chan Palomo <73038870+JulianChan123@users.noreply.github.com>
2023-05-27 15:25:15 -0600

    userRoutes

--- be/routes/api.php
+++ be/routes/api.php
@@ -19,8 +19,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
- 
+//Products routes
 Route::any('add', ProductController::class.'@add');
 Route::any('update', ProductController::class.'@update');
 Route::any('delete', ProductController::class.'@delete');
 Route::any('get', ProductController::class.'@get');
+
+//User routes
+Route::any('register', UserController::class.'@register');
+Route::any('login', UserController::class.'@login');
