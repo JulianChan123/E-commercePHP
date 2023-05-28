3801fc896af647fb56c7ad16754add3942a156e3
Julian Chan Palomo <73038870+JulianChan123@users.noreply.github.com>
2023-05-28 10:41:50 -0600

    productsServiceFinished

--- fe/src/app/services/product.service.ts
+++ fe/src/app/services/product.service.ts
@@ -1,27 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { productModel } from '../models/product.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  
+  AllProducts = new BehaviorSubject<productModel[]>([]);
+
   constructor(private http:HttpClient) { 
 
   }
 
   private baseUrl="http://localhost:8000/api/";
 
-  public add(form){
+  public add(form: any){
     return this.http.post(this.baseUrl+"add",form);
   }
-  public delete(id){
+  public delete(id: any){
     return this.http.post(this.baseUrl+"delete?id="+id,null);
   }
-  public update(form){
+  public update(form: any){
     return this.http.post(this.baseUrl+"update",form);
   }
-  public getFromDb(keys){
-    return this.http.post(this.baseUrl+"get?keys="+keys,null);
+  public getFromDb(keys: any){
+    return this.http.post(this.baseUrl+"get?keys="+keys,null).subscribe(res=>{
+      var r:any = res;
+      this.AllProducts.next(r.products)
+    });
   }
 }
